d20b6e2048f2b9f36058bcd657175212c01a21e1
Thekiddiejsandpython <bustida@gmail.com>
2017-02-23 15:15:07 -0700

    Update online-bootstrap-cdn.js

--- online-bootstrap-cdn.js
+++ online-bootstrap-cdn.js
@@ -2,20 +2,20 @@ function runcdn(){
 var xhr = new XMLHttpRequest();
 xhr.open("GET", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", false);
 
-xhr.send()
+xhr.send();
 //If CDN connection for Bootstrap is available (i.e, user is online), connect to CDN
-if(xhr.status.toString()[0] === 2){
+if(xhr.status.toString()[0] == 2){
   var a = document.createElement('link');
   a.rel = "stylesheet";
   a.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
-  a.integrity = "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u";
   a.crossorigin = "anonymous";
   document.head.appendChild(a);
   console.log("Connection Succesful!");
  }
   else{
-    console.log("Connection Unsuccesful!");
+    console.log("Connection Unsuccesful! Error code:" + xhr.status);
   }
 }
+
 //Else, proceed with the downloaded Bootstrap file
 runcdn();
