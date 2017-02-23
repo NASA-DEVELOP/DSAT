d4334889c534db97c6e8c672ee57836e1c7e2eed
Thekiddiejsandpython <bustida@gmail.com>
2017-02-23 14:57:11 -0700

    Add online support for Bootstrap
    If there is a successful connection to the bootstrap CDN, add the CDN connection to the document

---
+++ online-bootstrap-cdn.js
@@ -0,0 +1,14 @@
+var xhr = new XMLHttpRequest();
+xhr.open("GET", "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css", false);
+
+xhr.send()
+//If CDN connection for Bootstrap is available (i.e, user is online), connect to CDN
+if(xhr.status.toString()[0] === 2){
+  var a = document.createElement('link');
+  a.rel = "stylesheet";
+  a.href = "https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css";
+  a.integrity = "sha384-BVYiiSIFeK1dGmJRAkycuHAHRg32OmUcww7on3RYdg4Va+PmSTsz/K68vbdEjh4u";
+  a.crossorigin = "anonymous";
+  document.head.appendChild(a);
+}
+//Else, proceed with the downloaded Bootstrap file
